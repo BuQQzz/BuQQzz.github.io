aee4f23fe78e1814620793970273af41812fd76b
BuQQzz <buqqzz@gmail.com>
2023-07-20 12:05:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// script.js
+
 // Get the carousel container and the carousel track
 const carouselContainer = document.querySelector(".carousel-container");
 const carouselTrack = document.querySelector(".carousel-track");
@@ -21,10 +23,8 @@ function moveCarousel() {
   // Check if the first image is no longer visible on the left
   if (position <= -(carouselWidth * carouselImages.length)) {
     // Move the first image to the end of the track to create the infinite loop effect
-    carouselTrack.appendChild(carouselImages[0].cloneNode(true));
-    carouselTrack.removeChild(carouselImages[0]);
-    position += carouselWidth; // Adjust the position immediately
     carouselTrack.style.transition = "none"; // Disable the transition for immediate position update
+    position = 0; // Reset the position to the beginning
     carouselTrack.style.transform = `translateX(${position}px)`; // Move the carousel track to the adjusted position
     setTimeout(() => {
       carouselTrack.style.transition = "transform 0.1s"; // Re-enable the transition after immediate update
@@ -37,5 +37,3 @@ function moveCarousel() {
 
 // Call the moveCarousel function to start the animation
 moveCarousel();
-
-
