ce447a89876380a4d98b70e61ea7c95fa8c6bad4
BuQQzz <buqqzz@gmail.com>
2023-07-20 11:07:04 -0400

    update
    moved images and added carousel

---
+++ script.js
@@ -0,0 +1,34 @@
+// script.js
+
+// Get the carousel container and the carousel track
+const carouselContainer = document.querySelector(".carousel-container");
+const carouselTrack = document.querySelector(".carousel-track");
+
+// Get the individual images in the carousel
+const carouselImages = carouselTrack.querySelectorAll("img");
+
+// Calculate the total width of the carousel track
+const carouselWidth = carouselContainer.clientWidth;
+
+// Set the initial position of the carousel track
+let position = 0;
+
+// Function to move the carousel track
+function moveCarousel() {
+  // Move the carousel track to the left
+  position -= 1;
+  carouselTrack.style.transform = `translateX(${position}px)`;
+
+  // Check if the carousel has reached the end
+  if (position <= -carouselWidth) {
+    // Reset the position to the beginning
+    position = 0;
+    carouselTrack.style.transform = `translateX(${position}px)`;
+  }
+
+  // Call this function again after a short delay (e.g., 10ms)
+  requestAnimationFrame(moveCarousel);
+}
+
+// Call the moveCarousel function to start the animation
+moveCarousel();
